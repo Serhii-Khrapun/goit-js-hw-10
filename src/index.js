f871918f32ebfdda522a3f4e9837625b838ae167
Serhii-Khrapun <Sergiy.Khrapun@gmail.com>
2021-11-13 16:14:51 +0200

    закінчив частину ДЗ

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 import './css/styles.css';
 import countryCard from "./country-card.hbs";
+import debounce from "lodash.debounce";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const countryContainer = document.querySelector('.country-info');
 const searchInput = document.querySelector('#search-box');
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1000;
 
-searchInput.addEventListener("input",onSearch)
+searchInput.addEventListener("input",debounce(onSearch,DEBOUNCE_DELAY))
 
 function onSearch(event) {
-    event.preventDefault()
+   const searchCountry = searchInput.value;
+   fetchCountryByName(searchCountry)
+   .then(renderCountryCard)
+   .catch(showError)
+  
 }
-fetchCountryByName("ukraine")
- .then(renderCountryCard);
+
+function showError(params) {
+    Notify.failure("Oops, there is no country with that name");
+}
 
 function fetchCountryByName(countryName) {
     return fetch(
         `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages `,
-      ).then(response => response.json())
+      ).then(response => {
+          if(!response.ok){
+              throw Error(response.statusText)
+          }
+         return response.json()
+      })
 }
 function renderCountryCard(country) {
     const markup = countryCard(country)
